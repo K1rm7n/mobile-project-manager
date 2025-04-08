9aebc512c77a64015e9d7122c40247e2e96d4a43
Kirill Poliak <146371547+K1rm7n@users.noreply.github.com>
2025-04-08 21:47:09 +0300

    Create Color.kt

---
+++ ui/theme/Color.kt
@@ -0,0 +1,60 @@
+// Primary colors
+val PrimaryLight = Color(0xFF1976D2)
+val PrimaryVariantLight = Color(0xFF004BA0)
+val OnPrimaryLight = Color.White
+
+val PrimaryDark = Color(0xFF2196F3)
+val PrimaryVariantDark = Color(0xFF0069C0)
+val OnPrimaryDark = Color.Black
+
+// Secondary colors
+val SecondaryLight = Color(0xFF26A69A)
+val SecondaryVariantLight = Color(0xFF00766C)
+val OnSecondaryLight = Color.White
+
+val SecondaryDark = Color(0xFF4DB6AC)
+val SecondaryVariantDark = Color(0xFF00867D)
+val OnSecondaryDark = Color.Black
+
+// Background colors
+val BackgroundLight = Color(0xFFF5F5F5)
+val SurfaceLight = Color.White
+val OnBackgroundLight = Color(0xFF121212)
+val OnSurfaceLight = Color(0xFF121212)
+
+val BackgroundDark = Color(0xFF121212)
+val SurfaceDark = Color(0xFF1E1E1E)
+val OnBackgroundDark = Color.White
+val OnSurfaceDark = Color.White
+
+// Error colors
+val ErrorLight = Color(0xFFB00020)
+val OnErrorLight = Color.White
+
+val ErrorDark = Color(0xFFCF6679)
+val OnErrorDark = Color.Black
+
+// Task priority colors
+val LowPriorityColor = Color(0xFF8BC34A)
+val MediumPriorityColor = Color(0xFFFFC107)
+val HighPriorityColor = Color(0xFFFF9800)
+val UrgentPriorityColor = Color(0xFFF44336)
+
+// Task status colors
+val TodoStatusColor = Color(0xFFBDBDBD)
+val InProgressStatusColor = Color(0xFF42A5F5)
+val ReviewStatusColor = Color(0xFFAB47BC)
+val CompletedStatusColor = Color(0xFF66BB6A)
+
+// Project status colors
+val ActiveProjectColor = Color(0xFF4CAF50)
+val CompletedProjectColor = Color(0xFF9E9E9E)
+val ArchivedProjectColor = Color(0xFF607D8B)
+
+// Chart colors
+val ChartColor1 = Color(0xFF2196F3)
+val ChartColor2 = Color(0xFF4CAF50)
+val ChartColor3 = Color(0xFFFF9800)
+val ChartColor4 = Color(0xFFE91E63)
+val ChartColor5 = Color(0xFF9C27B0)
+val ChartColor6 = Color(0xFF3F51B5)
